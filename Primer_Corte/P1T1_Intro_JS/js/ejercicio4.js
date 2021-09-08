1cfcbcbd68f321bf2fe5177a15b4259037a8668e
Carlos David Páez Ferreira <carlos.paezf@usantoto.edu.co>
2021-09-08 08:00:32 -0500

    &&

--- Primer_Corte/P1T1_Intro_JS/js/ejercicio4.js
+++ Primer_Corte/P1T1_Intro_JS/js/ejercicio4.js
@@ -14,3 +14,21 @@ const calculadora = (op, a = 0, b = 0) => {
 }
 
 calculadora('+', 10);
+
+
+/* const calculadora = (op, a = 0, b = 0) => {
+    op = op || '+'
+    a = a && a
+    b = b && b
+    let res;
+    switch (op) {
+        case '+': res = a + b; break;
+        case '-': res = a - b; break;
+        case '*': res = a * b; break;
+        case '/': res = a / b; break;
+        default: break;
+    }
+    console.log(res)
+}
+
+calculadora(null, 10, 29); */
